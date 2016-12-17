0585bb43b586e0e69b9dfa183cd3273f3c68c549
ZKonstantinova <zori.konstantinova@gmail.com>
2016-12-17 10:19:38 +0200

    sections/categories edit/delete

--- BeerBlog/Models/BlogDbContext.cs
+++ BeerBlog/Models/BlogDbContext.cs
@@ -19,6 +19,8 @@ namespace BeerBlog.Models
 
         public virtual IDbSet<Category> Categories { get; set; }
 
+        public virtual IDbSet<Section> Sections { get; set; }
+
         public static BlogDbContext Create()
         {
             return new BlogDbContext();
